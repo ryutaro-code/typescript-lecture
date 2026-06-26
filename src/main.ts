3ae5fca40d98711c357903d1d627155f78c79649
ryutaro-web <k.ryutaro668@gmail.com>
2026-06-26 10:43:43 +0900

    main.tsの不要なインポートを削除

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-import { Mage } from "./mage";
-import { Warrior } from "./warrior";
-import { Character } from "./character.ts"
+import { Mage } from './mage';
+import { Warrior } from './warrior';
 
 // const character = new Character("太郎", 200); // abstract class はインスタンス化出来ないのでエラーが出る
 
-const warrior = new Warrior("アーサー", 100, "エクスカリバー");
+const warrior = new Warrior('アーサー', 100, 'エクスカリバー');
 warrior.showStatus();
 warrior.attack();
 
-const mage = new Mage("メディア", 80);
+const mage = new Mage('メディア', 80);
 mage.showStatus();
 mage.attack();
